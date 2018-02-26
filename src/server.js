dc2753fbb681d2214b2a72a98f2b9424154faddf
Samuel Cha <sunchipssam@gmail.com>
2018-02-26 11:13:46 -0800

    Tests passing

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -28,45 +30,41 @@ const queryAndThen = (query, res, cb) => {
   });
 };
 
-server.get('/accepted-answer/:soID', (req, res) => {
-  queryAndThen(Post.findOne({ soID: req.params.soID }), res, (post) => {
+const checkIfPostExists = (req, res, next) => {
+  queryAndThen(Post.findOne({ soID: req.params.soID }), res, post => {
     if (!post) {
       sendUserError("Couldn't find post with given ID", res);
       return;
     }
 
-    const query = Post.findOne({ soID: post.acceptedAnswerID });
-    queryAndThen(query, res, (answer) => {
-      if (!answer) {
-        sendUserError('No accepted answer', res);
-      } else {
-        res.json(answer);
-      }
-    });
+    req.post = post;
+    next();
+  });
+};
+
+server.get('/accepted-answer/:soID', checkIfPostExists, (req, res) => {
+  const query = Post.findOne({ soID: req.post.acceptedAnswerID });
+  queryAndThen(query, res, answer => {
+    if (!answer) {
+      sendUserError('No accepted answer', res);
+    } else {
+      res.json(answer);
+    }
   });
 });
 
-server.get('/top-answer/:soID', (req, res) => {
-  queryAndThen(Post.findOne({ soID: req.params.soID }), res, (post) => {
-    if (!post) {
-      sendUserError("Couldn't find post with given ID", res);
-      return;
+server.get('/top-answer/:soID', checkIfPostExists, (req, res) => {
+  const query = Post.findOne({
+    soID: { $ne: req.post.acceptedAnswerID },
+    parentID: req.post.soID,
+  }).sort({ score: 'desc' });
+
+  queryAndThen(query, res, answer => {
+    if (!answer) {
+      sendUserError('No top answer', res);
+    } else {
+      res.json(answer);
     }
-
-    const query = Post
-      .findOne({
-        soID: { $ne: post.acceptedAnswerID },
-        parentID: post.soID,
-      })
-      .sort({ score: 'desc' });
-
-    queryAndThen(query, res, (answer) => {
-      if (!answer) {
-        sendUserError('No top answer', res);
-      } else {
-        res.json(answer);
-      }
-    });
   });
 });
 
@@ -74,10 +72,7 @@ server.get('/popular-jquery-questions', (req, res) => {
   const query = Post.find({
     parentID: null,
     tags: 'jquery',
-    $or: [
-      { score: { $gt: 5000 } },
-      { 'user.reputation': { $gt: 200000 } }
-    ]
+    $or: [{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }],
   });
 
   queryAndThen(query, res, posts => res.json(posts));
@@ -86,12 +81,12 @@ server.get('/popular-jquery-questions', (req, res) => {
 server.get('/npm-answers', (req, res) => {
   const query = Post.find({
     parentID: null,
-    tags: 'npm'
+    tags: 'npm',
   });
 
-  queryAndThen(query, res, (posts) => {
+  queryAndThen(query, res, posts => {
     const answerQuery = Post.find({
-      parentID: { $in: posts.map(p => p.soID) }
+      parentID: { $in: posts.map(p => p.soID) },
     });
     queryAndThen(answerQuery, res, answers => res.json(answers));
   });
